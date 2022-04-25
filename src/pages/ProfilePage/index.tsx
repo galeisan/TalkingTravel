6e150e91195b4f8732b919374eacece4c5fc9257
galeisan <galeeva.leisan.27.09@yandex.ru>
2022-04-25 11:59:05 +0300

    add menu

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -2,46 +2,19 @@ import React, {useEffect, useRef, useState} from "react"
 import {observer} from "mobx-react";
 import {useNavigate} from "react-router";
 import {BaseLayout} from "../../components/BaseLayout";
-import {app, logout, useAuth, database} from "../../firebase";
+import {logout, useAuth} from "../../firebase";
 import Profile from "../../components/profile";
 import {Button} from "../../components/ui/Button";
+import {Menu} from "../../components/Menu";
 import styles from "./index.module.sass";
-import {collection} from "firebase/firestore";
-import {getDocs} from "firebase/firestore";
-import {getAllFeeds} from "../../utils/fetchData";
-import {Feed} from "../../components/Feed";
+import signup_img from "../../assets/signup_img.png";
+
 
 export const ProfilePage = observer(() => {
 
     const [loading, setLoading] = useState(false)
-    const [usersInfo, setUsersInfo] = useState<any>([]);
-
-    const usersDatabaseRef = collection(database, 'profile');
-    const currentUser = useAuth()
-
     let navigate = useNavigate()
-
-    if(loading) return <div>Загрузка...</div>
-
-    useEffect(()=>{
-
-    },[])
-
-
-    useEffect(() => {
-        const getUsersInfo = async () => {
-            const data = await getDocs(usersDatabaseRef)
-            let arr = data.docs.map((doc) => ({...doc.data()}))
-            let user = arr.findIndex(function (user,index){
-                return user.uid === currentUser.uid
-            })
-            let ans = arr[user]
-            console.log(ans)
-            setUsersInfo(ans)
-            console.log(usersInfo)
-        };
-        getUsersInfo().then(err => console.log(err));
-    }, [])
+    const currentUser = useAuth()
 
     async function handleLogout(){
         setLoading(true)
@@ -62,18 +35,19 @@ export const ProfilePage = observer(() => {
         <BaseLayout>
             <div className={styles.content_container}>
                 <div className={styles.content_wrapper}>
-                            <div className={styles.user_info_wrapper}>
-                                <img className={styles.user_avatar} src={currentUser?.photoURL} alt="IMAGE"/>
-                                <div className={styles.user_info}>
-                                    <h5>{usersInfo.name}</h5>
-                                    <Button onClick={handleChangeProfile} disabled={false} mode={"secondary"}>
-                                        Редактировать
-                                    </Button>
-                                </div>
-                            </div>
-                            <div className={styles.menu_container}>
-                                Мои места Подборки Лайки Прочее
-                            </div>
+                    <div className={styles.user_info_wrapper}>
+                        <img className={styles.user_avatar} src={currentUser?.photoURL} alt="IMAGE"/>
+                        <div className={styles.user_info}>
+                            <h1 className={styles.user_name}>
+                                {currentUser?.name}
+                            </h1>
+                            <p className={styles.user_email}>
+                                {currentUser?.email}
+                            </p>
+                            <Button onClick={handleChangeProfile} disabled={false} mode={"secondary"}>Редактировать</Button>
+                        </div>
+                    </div>
+                    <Menu/>
                     {currentUser &&
                     <>
                         <Profile/>
